9b8ddd7b9495255a2545c42e57699b27b1167e7e
Seeun Kim <ubbke66@naver.com>
2022-07-21 08:52:36 +0900

    modified files and added files

---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+	<h1>Hello {{ userName }}</h1>
+	<p></p>
+</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			userName: "John Doe",
+			message: '',
+			arr: [],
+			obj: {}
+		}
+	}
+}
+</script>
